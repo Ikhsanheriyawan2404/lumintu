8f5ec8b01b8f5bcfc4fa0a6820892b9be23504a0
Sulthan Alif Hayatyo <sulthanalif45@gmail.com>
2023-04-06 02:15:22 +0700

    Verif pembayaran

---
+++ resources/views/payment_verif/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card h-100">
+              <div class="card-header pb-0 p-3">
+                <div class="row">
+                  <div class="col-6 d-flex align-items-center">
+                    <h6 class="mb-0">Verifikasi Pembayaran</h6>
+                  </div>
+                  <div class="col-6 text-end">
+                  </div>
+                </div>
+              </div>
+              <div class="card-body p-3 pb-0">
+                data table
+              </div>
+            </div>
+          </div>
+    </div>
+</div>
+@endsection
