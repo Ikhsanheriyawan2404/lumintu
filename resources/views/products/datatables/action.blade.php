ed4cd2d70b5face52cdafc5fbc232a0f9a146a75
loronghitam <sutio.mudianro@gmail.com>
2023-04-06 21:38:41 +0700

    fix(hosting): modify gitignore
    kebutuhan hosting
    
    [skip ci]

--- resources/views/products/datatables/action.blade.php
+++
@@ -1,6 +0,0 @@
-<form id="deleteDoc" method="post">
-    @csrf
-    @method('DELETE')
-    <a href="javascript:void()" data-id="{{ $row->id }}" id="editItem" class="btn btn-sm btn-primary mb-0">Edit</a>
-    <button type="submit" class="btn btn-sm btn-danger mb-0 deleteBtn" data-id="{{ $row->id }}">Hapus</button>
-</form>
