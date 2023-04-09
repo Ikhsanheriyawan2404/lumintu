6d4681c83d533c572c1726882ee6f0d678a74587
Muh Hilman Sholehudin <ahilman3107@gmail.com>
2023-04-10 05:40:47 +0700

    Update Login & Landing Page

--- resources/views/landing/layouts/footer.blade.php
+++ resources/views/landing/layouts/footer.blade.php
@@ -1,13 +1,86 @@
-<!-- Footer-->
-<footer class="bg-black text-center py-5">
-    <div class="container px-5">
-        <div class="text-white-50 small">
-            <div class="mb-2">&copy; CV. Lumintu SIP 2023. All Rights Reserved.</div>
-            <a href="#!">Privacy</a>
-            <span class="mx-1">&middot;</span>
-            <a href="#!">Terms</a>
-            <span class="mx-1">&middot;</span>
-            <a href="#!">FAQ</a>
-        </div>
-    </div>
-</footer>
+<div class="container">
+    <div class="subscribe-area wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="subscribe-content mt-45">
+                    <h2 class="subscribe-title">Berlangganan sekarang<span>Dapatkan pembaruan rutin</span></h2>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="subscribe-form mt-50">
+                    <form action="#">
+                        <input type="text" placeholder="Masukkan Email">
+                        <button class="main-btn">Berlangganan</button>
+                    </form>
+                </div>
+            </div>
+        </div> <!-- row -->
+    </div> <!-- subscribe area -->
+    <div class="footer-widget pb-100">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-8">
+                <div class="footer-about mt-50 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
+                    <a class="logo" href="/">
+                        <img src="{{ asset('assets/img/logo2.png') }}" alt="logo">
+                    </a>
+                    <ul class="social">
+                        <li><a href="#"><i class="lni-facebook-filled"></i></a></li>
+                        <li><a href="#"><i class="lni-twitter-filled"></i></a></li>
+                        <li><a href="#"><i class="lni-instagram-filled"></i></a></li>
+                        <li><a href="#"><i class="lni-linkedin-original"></i></a></li>
+                    </ul>
+                </div> <!-- footer about -->
+            </div>
+            <div class="col-lg-5 col-md-7 col-sm-7">
+                <div class="footer-link d-flex mt-50 justify-content-md-between">
+                    <div class="link-wrapper wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s">
+                        <div class="footer-title">
+                            <h4 class="title">Akses Cepat</h4>
+                        </div>
+                        <ul class="link">
+                            <li><a href="/">Profile Perusahaan</a></li>
+                            <li><a href="/">Detail Program</a></li>
+                            <li><a href="/">Syarat & Ketentuan</a></li>
+                        </ul>
+                    </div> <!-- footer wrapper -->
+                    <div class="link-wrapper wow fadeIn" data-wow-duration="1s" data-wow-delay="0.6s">
+                        <div class="footer-title">
+                            <h4 class="title">Menu</h4>
+                        </div>
+                        <ul class="link">
+                            <li><a href="#home">Beranda</a></li>
+                            <li><a href="#profile">Tentang</a></li>
+                            <li><a href="#program">Program</a></li>
+                            <li><a href="#gallery">Galeri</a></li>
+                            <li><a href="#contact">Kontak</a></li>
+                        </ul>
+                    </div> <!-- footer wrapper -->
+                </div> <!-- footer link -->
+            </div>
+            <div class="col-lg-3 col-md-5 col-sm-5">
+                <div class="footer-contact mt-50 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+                    <div class="footer-title">
+                        <h4 class="title">Hubungi Kami</h4>
+                    </div>
+                    <ul class="contact">
+                        <li>+6282110271556</li>
+                        <li>lumintu.laundry123@gmail.com</li>
+                        <li>Kampung Baru No. 1A RT.007 RW.003 Dangdang Cisauk Kab. Tangerang, Banten.</li>
+                    </ul>
+                </div> <!-- footer contact -->
+            </div>
+        </div> <!-- row -->
+    </div> <!-- footer widget -->
+    <div class="footer-copyright">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="copyright d-sm-flex justify-content-between">
+                    <div class="copyright-content">
+                        <p class="text">Created by <a href="#" rel="nofollow">CV. Lumintu SIP</a></p>
+                    </div> <!-- copyright content -->
+                </div> <!-- copyright -->
+            </div>
+        </div> <!-- row -->
+    </div> <!-- footer copyright -->
+</div> <!-- container -->
+<div id="particles-2"></div>
