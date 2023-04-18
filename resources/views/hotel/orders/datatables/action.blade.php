9b4f86ac2ebbb94be13e8f99c38218eba0521183
loronghitam <sutio.mudianro@gmail.com>
2023-04-18 20:26:11 +0700

    fix(hotel): buttun
    [skip ci]

--- resources/views/hotel/orders/datatables/action.blade.php
+++ resources/views/hotel/orders/datatables/action.blade.php
@@ -2,10 +2,12 @@
     @csrf
     @method('DELETE')
     <a href="{{ route('orders.show', $row->id) }}" class="btn btn-sm btn-primary mb-0"><i class="fa fa-eye"></i></a>
-    <a href="{{ route('orders.show', $row->id) }}" class="btn btn-sm btn-primary mb-0"><i
-            class="fa fa-dollar-sign"></i></a>
-    <a href="{{ route('orders.edit', $row->id) }}" class="btn btn-sm btn-primary mb-0"><i
-            class="fa fa-pencil-alt"></i></a>
-    <button type="submit" class="btn btn-sm btn-danger mb-0 deleteBtn" data-id="{{ $row->id }}"><i
-            class="fa fa-trash"></i></button>
+        @if($row->status->value == 'pending')
+        <a href="{{ route('orders.edit', $row->id) }}" class="btn btn-sm btn-primary mb-0"><i
+                class="fa fa-pencil-alt"></i></a>
+        <button type="submit" class="btn btn-sm btn-danger mb-0 deleteBtn" data-id="{{ $row->id }}"><i
+                class="fa fa-trash"></i></button>
+        @endif
+
+
 </form>
